a60b17841bb38b7b8a0048379bfd2fb81a2e39d9
Bendik Solheim <bendik@solheim.im>
2015-10-24 14:38:06 +0200

    Working version with map and button to center on TRD

--- Faggruppe/ViewController.swift
+++ Faggruppe/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var mapView : MKMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView = MKMapView(frame: view.bounds)
+        view.addSubview(mapView!)
+        
+        let centerButton = UIButton(type: UIButtonType.InfoDark)
+        centerButton.addTarget(self, action: "centerHome:", forControlEvents: UIControlEvents.TouchUpInside)
+        centerButton.center = CGPointMake(CGRectGetMaxX(view.frame) - 20.0, CGRectGetMaxY(view.frame) - 20.0)
+        view.addSubview(centerButton)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func centerHome(sender: UIButton) {
+        let location = CLLocation(latitude: 63.3985591, longitude: 10.3940111)
+        centerMapOn(location)
     }
-
-
+    
+    let regionRadius : CLLocationDistance = 1000
+    
+    func centerMapOn(location: CLLocation) {
+        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        print(region)
+        mapView?.setRegion(region, animated: true)
+    }
 }
 
